6ed666a96323ee81f1b5a8202bf0606ef08f1f55
George R. Boguchevski B. <georgenatalinobbb@gmail.com>
2025-06-03 16:03:52 -0300

    server.js

--- reactAula/backend/server.js
+++ reactAula/backend/server.js
@@ -1,22 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-const sqlite3 = require('sqlite3').verbose();
-
+const express = require('express')
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3')
 const app = express();
 const db = new sqlite3.Database('./database.sqlite');
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// Criar tabela se não existir
-db.run(`CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+db.run(`CREATE TABLE ID NOT EXISTS users(
+    id INT PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     email TEXT
 )`);
 
-// Rota GET (listar usuários)
 app.get('/users', (req, res) => {
     db.all('SELECT * FROM users', (err, rows) => {
         if (err) res.status(500).send(err);
@@ -24,7 +22,6 @@ app.get('/users', (req, res) => {
     });
 });
 
-// Rota POST (adicionar usuário)
 app.post('/users', (req, res) => {
     const { name, email } = req.body;
     db.run(`INSERT INTO users (name, email) VALUES (?, ?)`, [name, email], function(err) {
