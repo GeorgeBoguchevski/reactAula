40f467a39eb34fd7dd471577c393c61d72596c33
George R. Boguchevski B. <georgenatalinobbb@gmail.com>
2025-06-03 16:29:13 -0300

    server.js

--- reactAula/backend/server.js
+++ reactAula/backend/server.js
@@ -1,36 +1,34 @@
-const cors = require('cors');
-const express = require('express')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const sqlite3 = require('sqlite3')
-const app = express();
-const db = new sqlite3.Database('./database.sqlite');
+const app = express()
+const db = new sqlite3.Database('./database.sqlite')
+const cors = require('cors')
+const express = require('express')
 
-app.use(cors());
-app.use(bodyParser.json());
-
+app.use(cors())
+app.use(bodyParser.json())
 
 db.run(`CREATE TABLE ID NOT EXISTS users(
     id INT PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     email TEXT
-)`);
+)`)
 
 app.get('/users', (req, res) => {
-    db.all('SELECT * FROM users', (err, rows) => {
-        if (err) res.status(500).send(err);
-        else res.send(rows);
-    });
-});
+    db.all('SELECT * FROM users', (err, rows) =>{
+        if(err) res.status(500).send(err);
+        else res.send(rows)
+    })
+})
 
 app.post('/users', (req, res) => {
-    const { name, email } = req.body;
-    db.run(`INSERT INTO users (name, email) VALUES (?, ?)`, [name, email], function(err) {
+    const { name, email } = req.body
+    db.run('INSERT  INTO users (name, email) VALUES (?, ?)', [name, email], function( err) {
         if (err) res.status(500).send(err);
         else res.send({ id: this.lastID, name, email });
-    });
-});
-
+    })
+})
 const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`API rodando em http://localhost:${PORT}`);
-});
+    console.log(`API rodando em http://localhost${PORT}`)
+})
